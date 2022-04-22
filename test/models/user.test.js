a311a87c24dc50ae9775ffc5d9d9643a56c194d4
Carlos Javier Loya Amaya <carlos.loya@Abitat.com.mx>
2022-04-21 22:18:16 -0600

    Quitando parametros a Unit Test

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -3,12 +3,12 @@ const User = require('./../../app/models/user')
 describe("Unit Tests for User class", () => {
 
     test('1) Create an User object', () => {
-        const user = new User(1, "CarlosL", "Carlos", "Bio", "updateCreated", "lastUpdated")
+        const user = new User(1, "CarlosL", "Carlos", "Bio")
         expect(user.id).toBe(1)
         expect(user.username).toBe("CarlosL")
         expect(user.name).toBe("Carlos")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("updateCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
     })
 })
